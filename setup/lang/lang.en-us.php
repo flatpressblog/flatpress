6be77d10dfb71b66548b5ca7624b932552d4751c
real_nowhereman <real_nowhereman@aa359531-4221-0410-9edc-b0b45c38dadb>
2007-11-02 16:54:01 +0000

    added sample content generation to setup, fixed h4 in leggero/res/common.css

--- setup/lang/lang.en-us.php
+++ setup/lang/lang.en-us.php
@@ -81,5 +81,85 @@ LangId: English
 	$lang['buttonbar'] = array(
 		'next'	=> 'Next >'
 	);
+	
+	$lang['samplecontent'] = array();
+	
+	$lang['samplecontent']['menu']['subject'] = 'Menu'; 
+	$lang['samplecontent']['menu']['content'] = <<<MENU
+[list]
+[*][url=?]Home[/url]
+[*][url=?paged=1]Blog[/url]
+[*][url=static.php?page=about]About[/url]
+[*][url=contact.php]Contact[/url]
+[/list]
+MENU;
+
+	$lang['samplecontent']['entry']['subject'] = 'Welcome to FlatPress!' ;
+	$lang['samplecontent']['entry']['content'] = <<<CONT
+This is a sample entry, posted to show you some of the features of [url=http://www.flatpress.org]FlatPress[/url].
+
+The more tag allows you to create a "jump" between an excerpt and the complete article.
+
+[more] 
+
+
+[h4]Styling[/h4]
+
+The default way to style and format your content is [url=http://wiki.flatpress.org/doc:plugins:bbcode]BBcode[/url] (bulletin board code). BBCode is an easy way to style your posts. Most common codes are allowed. Like [b] for [b]bold[/b] (html: strong), [i] for [i]italics[/i] (html: em), etc.
+
+[quote]
+There are also [b]quote[/b] blocks to display your favourite quotations. 
+[/quote]
+
+[code]
+and 'code' displays your snippets in a monospaced fashion
+[/code]
+
+img and url tag have also special options. You can find out more on the [url=http://www.flatpress.org]FP official website[/url].
+
+[h4]Entries (posts) and Static pages[/h4]
+
+This is an entry, while [url=static.php?page=about]About[/url] is a [b]static page[/b]. A static page is an entry (a post) which cannot be commented, and which does not appear together with the normal posts of the blog.
+
+Static pages are useful to create general information pages; with this new version of FlatPress you are allowed to make one of these pages the [b]opening page[/b] for your visitors. This means that now with FlatPress you can now run a complete non-blog site. The option to make a static page your start page is in the [b]option panel[/b] of the [url=admin.php]admin area[/url]   
+
+
+[h4]Plugins[/h4]
+
+FlatPress is very customizable, and supports [url=http://wiki.flatpress.org/doc:plugins]plugins[/url] to extend its power. This BBCode plugin is itself a plugin!
+
+We have created some more sample content, to show you some of the FP well hidden functions and gems :) ; you can find two [b]static pages[/b] ready to accept your contents ([url=static.php?page=about]About me[/url]) and links ([url=static.php?page=menu]Menu[/url]): notice that your links will appear on your sidebar as well, this is the magic of the [b]blockparser widget[/b]: see the [url=http://wiki.flatpress.org/doc:faq]FAQ[/url] for this and more. 
+
+[h4]Widgets[/h4]
+
+There isn't a single fixed element in the sidebar(s). All the elements you can find in the bars sourrounding this text are completely ositionable, and most of them are customizable as well. Some of theme provide even a panel interface in the admin area.  
+
+These elements are called [url=http://wiki.flatpress.org/doc:widgets]widgets[/url].
+
+For more on widgets and [url=http://wiki.flatpress.org/doc:tips:widgets]some tips[/url] to get nice effects, read the [url=http://wiki.flatpress.org/]wiki[/url]
+
+[h4]See more[/h4]
+
+Want to see more?
+
+[list]
+[*]Follow the [url=http://www.flatpress.org/home/blog.php]official blog[/url] to know what's going on in the FlatPress world
+[*]Go to the [url=http://www.flatpress.org/forums/categories.php]forums[/url] for support and chit-chat
+[*]Get [b]great themes[/b] on the [url=http://themes.flatpress.org]editor's pick[/url] site or choose from [url=http://wiki.flatpress.org/res:themes]other users' submissions[/url]!
+[*]Check out the [url=http://wiki.flatpress.org/res:plugins]unofficial plugins[/url]
+[/list]
+
+Now you can [url=login.php]Login[/url] to get to the [url=admin.php]Control Panel[/url] and start posting!
+
+Have fun! :) 
+
+[i][url=http://www.nowhereland.it]NoWhereMan[/url] and the FlatPress Team[/i]
+	
+CONT;
+
+
+	$lang['samplecontent']['about']['subject']	= 'About';
+	$lang['samplecontent']['about']['content']	= 
+		"Write something about yourself here ([url=admin.php?p=static&action=write&page=about]Edit me![/url])";
 
 ?>
--- setup/lib/step2.lib.php
+++ setup/lib/step2.lib.php
@@ -1,7 +1,29 @@
 <?php
 
 	function check_step() {
-		return validate();
+	
+		global $lang;
+		
+		$validate = validate();
+		
+		if ($validate) {
+		
+			$vl =& $lang['samplecontent'];
+			entry_save(array(
+				'subject' => $vl['entry']['subject'],
+				'content' => $vl['entry']['content']
+			));	
+			static_save(array(
+				'subject' => $vl['menu']['subject'],
+				'content' => $vl['menu']['content']
+			), 'menu');
+			static_save(array(
+				'subject' => $vl['about']['subject'],
+				'content' => $vl['about']['content']
+			), 'about');
+		}
+		
+		return $validate;
 	}
 
 ?>
