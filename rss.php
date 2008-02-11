509efe2188d73a2d5c2cfe0031bff8d5f89857c9
real_nowhereman <real_nowhereman@aa359531-4221-0410-9edc-b0b45c38dadb>
2008-02-11 07:50:28 +0000

    now redirecting to the correct page. KNOWN ISSUE: on 404 (no content) output is html!

--- rss.php
+++ rss.php
@@ -5,8 +5,7 @@
 	require_once (INCLUDES_DIR.'includes.php');
 
 	/* backward compatibility */
-	
 	@utils_status_header(301);
-	@utils_redirect('?'. str_replace('&amp;','&', theme_feed_link()) . '&' . $_SERVER['QUERY_STRING']);	
+	@utils_redirect(str_replace('&amp;','&', theme_feed_link()) . '&' . $_SERVER['QUERY_STRING'], true);	
 
 ?>
