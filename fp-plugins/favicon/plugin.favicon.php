c173e4943ca8a31f1ae3456367cfcbb0e815d225
real_nowhereman <real_nowhereman@aa359531-4221-0410-9edc-b0b45c38dadb>
2010-09-01 06:35:44 +0000

    the favicon image MUST be named "favicon.ext"! (or IE won't show it...)

--- fp-plugins/favicon/plugin.favicon.php
+++ fp-plugins/favicon/plugin.favicon.php
@@ -9,7 +9,10 @@ Author URI: http://www.nowhereland.it/
 */ 
  
 function plugin_favicon_head() {
-	echo '<link rel="shortcut icon" href="' .  plugin_geturl('favicon') .'imgs/fplogo.gif" />';
+	// your file *must* be named favicon.ext, 
+	// where ext is an image extension (such as gif, png, ico...)
+	echo '<link rel="shortcut icon" href="' .  
+		plugin_geturl('favicon') .'imgs/favicon.gif" />';
 }
  
 add_action('wp_head', 'plugin_favicon_head');
