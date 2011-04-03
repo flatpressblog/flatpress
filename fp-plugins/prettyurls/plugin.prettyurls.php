ee6e7c551c647de636f58251525816ed31a87153
real_nowhereman <real_nowhereman@aa359531-4221-0410-9edc-b0b45c38dadb>
2011-04-03 17:13:12 +0000

    nasty auth bug

--- fp-plugins/prettyurls/plugin.prettyurls.php
+++ fp-plugins/prettyurls/plugin.prettyurls.php
@@ -251,7 +251,8 @@ class Plugin_PrettyURLs {
 		global $fp_params;
 		
 		$this->fp_params =& $fp_params;
-		$this->baseurl = PRETTYURLS_PATHINFO? BLOG_BASEURL . 'index.php/' : BLOG_BASEURL;
+		// $this->baseurl = PRETTYURLS_PATHINFO? BLOG_BASEURL . 'index.php/' : BLOG_BASEURL;
+		$this->baseurl = BLOG_BASEURL . '?u=/';
 	
 		if (PRETTYURLS_TITLES) {
 			#if ($f = io_load_file(PRETTYURLS_CACHE))
@@ -271,13 +272,16 @@ class Plugin_PrettyURLs {
 
 		# this is not working if you reach flatpress via symlink
 		# unless you don't edit manually defaults.php
-		if (strpos($_SERVER['REQUEST_URI'], BLOG_ROOT)!==false) {
-			$url = $_SERVER['REQUEST_URI'];
-			$del = BLOG_ROOT;
-			if (strpos($url, 'index.php')!==false)
-				$del = $del . 'index.php/';
-			$url = substr($url, strlen($del)-1);
-		}
+		//if (strpos($_SERVER['REQUEST_URI'], BLOG_ROOT)!==false) {
+		//	$url = $_SERVER['REQUEST_URI'];
+		//	$del = BLOG_ROOT;
+		//	if (strpos($url, 'index.php')!==false)
+		//		$del = $del . 'index.php/';
+		//	$url = substr($url, strlen($del)-1);
+		//}
+		
+		$url = $_GET['u'];
+
 		
 		// removes querystrings
 		if (false !== $i = strpos($url, '?'))
