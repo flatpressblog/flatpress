f97f35dd5e569709f978729eb4a1eda34e2e6db3
liquibyte <liquibyte@gmail.com>
2014-01-29 23:43:52 -0500

    Doing a re-commit to make sure things are working right. Signed-off-by: liquibyte <liquibyte@gmail.com>

--- admin/panels/widgets/admin.widgets.js
+++ admin/panels/widgets/admin.widgets.js
@@ -1,9 +1,8 @@
 /*
- * Flatpress widget js admin
- * Based on original flatpress' code
+ * nibble widget js admin
+ * Based on original nibble' code
  * Require jQuery and jQuery UI (Core, Draggable, Droppable and Effects Core)
  * Coded by Piero VDFN <vogliadifarniente@gmail.com>
-<<<<<<< HEAD
  * Re-Coded by liquibyte <liquibyte@gmail.com>
  *     Colors weren't resetting on mouseout and position:absolute didn't work.
  *     Position:absolute changed to position:fixed.  I also changed the
@@ -13,7 +12,7 @@
  *     accepts drag and drop from installed widgets to remove.
  * Released under GNU GPL v2
  */
-var FlatPress = {
+var nibble = {
 	winstancedrag : function() {
 		$('.widget-class').draggable({
 			'scroll' : true,
@@ -68,7 +67,7 @@ var FlatPress = {
 				if (parent.children().length < 1) {
 					parent.append('<li class="widget-placeholder">Drop here</li>');
 				}
-				FlatPress.wreload();
+				nibble.wreload();
 			}
 		});
 	},
@@ -95,7 +94,7 @@ var FlatPress = {
 				if (parent.children().length < 1) {
 					parent.append('<li class="widget-placeholder">Drop here</li>');
 				}
-				FlatPress.wreload();
+				nibble.wreload();
 			}
 		});
 	},
@@ -121,7 +120,7 @@ var FlatPress = {
 				setTimeout(function() {
 					draggable.remove();
 				});
-				FlatPress.wreload();
+				nibble.wreload();
 			}
 		});
 		$('.widget-class').droppable({
@@ -145,7 +144,7 @@ var FlatPress = {
 				setTimeout(function() {
 					draggable.remove();
 				});
-				FlatPress.wreload();
+				nibble.wreload();
 			}
 		});
 
@@ -156,144 +155,4 @@ var FlatPress = {
 		this.wplaceholder();
 	}
 }
-FlatPress.wreload();FlatPress.wtrash();
-=======
- * Released under GNU GPL v2
- */
-
-var FlatPress = {
-wclass: function() {
-	$('.widget-class').draggable({
-		'scroll' : true,
-		'helper':function(event) {
-			return $(this).clone().appendTo('body').removeClass('widget-class').css({
-					'position': 'absolute',
-					'opacity' : 0.7,
-					'background-color' : '#b31',
-					'top' : event.pageY-10,
-					'left' : event.pageX-($(this).width()/4),
-					'list-style-type' : 'none',
-					'width' : $(this).width()
-			}).addClass('widget-dragger');
-		}
-	});
-},
-
-winstancedrag: function() {
-	$('.widget-instance').draggable({
-		'scroll' : true,
-		'helper':function(event) {
-			return $(this).clone().appendTo('body').removeClass('widget-class').css({
-					'position': 'absolute',
-					'opacity' : 0.7,
-					'background-color' : '#b31',
-					'list-style-type' : 'none',
-					'width' : $(this).width()
-			}).addClass('widget-dragger');
-		}
-	});
-},
-wplaceholder: function() {
-	$('.widget-placeholder').droppable({
-		'accept' : '.widget-class, .widget-dragger, .widget-instance',
-		'over' : function(event, ui) {
-			$(this).animate({'background-color' : '#78ba91'})
-		},
-		'out' : function(event, ui) {
-			$(this).animate({'background-color' : '#fff'})
-		},
-		'drop' : function(event, ui) {
-			var parent=ui.draggable.parent();
-			var where=$(this).parent().attr('id').split('-')[1];
-			var replace = null;
-			if(ui.draggable.hasClass('widget-instance')) {
-				replace=ui.draggable;
-			} else {
-				replace=$('<li class="'+ui.draggable.attr('class')+'"></li>').append(ui.draggable.children().clone());
-				replace.removeClass('widget-class').addClass('widget-instance');
-			}
-			replace.children('input').attr('name', 'widgets['+where+'][]');
-			$(this).replaceWith(replace);
-			if(parent.children().length<1) {
-				parent.append('<li class="widget-placeholder">Drop here</li>');
-			}
-			FlatPress.wreload();
-		}
-	});
-},
-winstancedrop: function() {
-	$('.widget-instance').droppable({	
-		'accept' : '.widget-class, .widget-dragger, .widget-instance',
-		'over' : function(event, ui) {
-			$(this).animate({'background-color' : '#78ba91'})
-		},
-		'out' : function(event, ui) {
-			$(this).animate({'background-color' : '#fff'})
-		},
-		'drop' : function(event, ui) {
-			var parent=ui.draggable.parent();
-			var where=$(this).parent().attr('id').split('-')[1];
-			var replace = null;
-			if(ui.draggable.hasClass('widget-instance')) {
-				replace=ui.draggable;
-			} else {
-				replace=$('<li class="'+ui.draggable.attr('class')+'"></li>').append(ui.draggable.children().clone());
-				replace.removeClass('widget-class').addClass('widget-instance');
-			}
-			replace.children('input').attr('name', 'widgets['+where+'][]');
-			$(this).after(replace);
-			$(this).animate({'background-color' : '#fff'});
-			if(parent.children().length<1) {
-				parent.append('<li class="widget-placeholder">Drop here</li>');
-			}
-			FlatPress.wreload();
-		}
-	});
-},
-wtrash: function() {
-	$('#widget-trashcan').droppable({
-		'accept' : '.widget-instance',
-		'over' : function(event, ui) {
-			$(this).animate({'background-color' : '#faa'})
-		},
-		'out' : function(event, ui) {
-			$(this).animate({'background-color' : '#a22'})
-		},
-		'drop' : function(event, ui) {
-			var parent=ui.draggable.parent();
-			var draggable = $(ui.draggable);
-
-			// we can't remove() draggable here, because of a bug with jquery UI + IE8
-			// we'll defer it
-			draggable.fadeOut();
-			$('.widget-dragger').remove();
-
-			// last element has not been removed, 
-			// so there is still one in the list, soon to be deleted '
-			// (parent.children().lenght==1)
-			if(parent.children().length<2) {
-				parent.append('<li class="widget-placeholder">Drop here</li>');
-			}
-			$(this).animate({'background-color' : '#a22'});
-
-			// deferred removal takes place here
-			setTimeout(function() {
-				draggable.remove();
-			});
-
-			FlatPress.wreload();
-		}
-	});
-},
-wreload: function(){
-	this.wclass();
-	this.winstancedrag();
-	this.wplaceholder();
-	this.winstancedrop();
-	//wtrash();
-}
-}
-//$(document).ready(wreload);
-FlatPress.wreload();FlatPress.wtrash();
-
->>>>>>> 109664842ba0aaec1b8e462a3fdcee470110a499
+nibble.wreload();nibble.wtrash();
