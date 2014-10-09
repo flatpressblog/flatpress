d600ec2e468aa4faaa8d0e9d19cb0f6743de3437
MarcThibeault <info@marcthibeault.com>
2014-10-08 22:17:43 -0400

    First commit for Leggero-v2

---
+++ fp-interface/themes/leggero/leggero-v2/style.conf.php
@@ -0,0 +1,25 @@
+<?php
+/*  
+Style Name: Leggero V2
+Style URI: http://www.flatpress.org/
+Description: Modern and actual version of the default FlatPress theme. 
+Version: 0.705
+Author: NoWhereMan, Drudo and Marc Thibeault
+Author URI: http://www.flatpress.org/
+*/
+
+
+
+	
+	$style['name'] = 'leggero-v2';
+	$style['author'] = 'NoWhereMan';
+	$style['www'] = 'http://www.flatpress.org/';
+	
+	$style['version'] = 0.705;
+		
+	$style['style_def'] = 'style.css';
+	$style['style_admin'] = 'admin.css';
+	$style['style_print'] = 'print.css';
+	$style['style'] = 'default';
+	
+?>
