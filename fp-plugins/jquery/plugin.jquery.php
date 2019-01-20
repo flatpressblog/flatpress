74630a2286d08274fd7fe3dc9b52cf5fcdfd769d
azett <githubregistrierung@azett.com>
2019-01-20 16:13:36 +0100

    jQuery plugin does load jquery files locally now

--- fp-plugins/jquery/plugin.jquery.php
+++ fp-plugins/jquery/plugin.jquery.php
@@ -1,12 +1,12 @@
 <?php
 /*
 Plugin Name: jQuery
-Version: 2.0
+Version: 2.0.1
 Plugin URI: http://www.vdfn.altervista.org/
-Description: provides <a href="http://jquery.com/" title="jQuery">jQuery</a>
+Description: Provides <a href="http://jquery.com/" title="jQuery">jQuery</a> locally.
 Author: Piero VDFN
 Author URI: http://www.vdfn.altervista.org/
-JQuery and JQueryUI version bump by liquibyte
+JQuery and JQueryUI version bump by Arvid Zimmermann
 */
 
 ## Original author: NoWhereMan (http://www.nowhereland.it)
@@ -19,10 +19,8 @@ function plugin_jquery_head() {
 	$pdir=plugin_geturl('jquery');
 	echo <<<JSUTILS
 	<!-- start of jsUtils -->
-	<!--<script type="text/javascript" src="{$pdir}res/jquery-1.7.1.min.js"></script>-->
-	<!--<script type="text/javascript" src="{$pdir}res/jquery-ui-1.8.11.custom.min.js"></script>-->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+	<script type="text/javascript" src="{$pdir}res/jquery/1.10.2/jquery.min.js"></script>
+	<script type="text/javascript" src="{$pdir}res/jqueryui/1.10.3/jquery-ui.min.js"></script>
 	<!-- end of jsUtils -->
 JSUTILS;
 }
