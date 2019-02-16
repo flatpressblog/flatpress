fb934477d60b5da07f01259e15831248bb4af1ec
azett <githubregistrierung@azett.com>
2019-02-16 17:34:58 +0100

    fixed little syntax glitch

--- fp-plugins/prettyurls/lang/lang.cs-cz.php
+++ fp-plugins/prettyurls/lang/lang.cs-cz.php
@@ -1,36 +1,34 @@
 ﻿<?php
-	$lang['plugin']['prettyurls']['errors'] = array (
-		-2	=> 'Nelze najít nebo vytvořit soubor <code>.htaccess</code> ve Vašem kořenovém '.
-				'adresáři. PrettyURLs nemusí fungovat správně, viz Nastavení PretyURLs.'
-	);
-	
-	$lang['admin']['plugin']['submenu']['prettyurls'] = 'PrettyURLs Config';
-	$lang['admin']['plugin']['prettyurls'] = array(
-		'head'		=> 'Nastavení PrettyURLs',
-		'htaccess'	=> '.htaccess',
-		'description'=>'Tímto editorem můžete editovat Váš soubor'.
-						'<code><a class="hint" href="https://notepad-plus-plus.org/">.htaccess</a></code>.',
-		'cantsave'	=> ''Nemůžete editovat tento soubor, protože není <strong>zapisovatelný</strong>. Můžete nastavit povolení k zapisování nebo kopírovat a vložit do souboru a poté nahrát.',
-		'mode'		=> 'Režim',
-		'auto'		=> 'Automatický',
-			'autodescr'	=> 'se snaží vybrat nejlepší volbu',
-		'pathinfo'	=> 'Path Info',
-			'pathinfodescr' => 'např. /index.php/2011/01/01/hello-world/',
-		'httpget'	=> 'HTTP Get',
-			'httpgetdescr'=> 'např. /?u=/2011/01/01/hello-world/',
-		'pretty'	=> 'Pretty',
-			'prettydescr'=> 'např. /2011/01/01/hello-world/',
+$lang ['plugin'] ['prettyurls'] ['errors'] = array(
+	-2 => 'Nelze najít nebo vytvořit soubor <code>.htaccess</code> ve Vašem kořenovém ' . 'adresáři. PrettyURLs nemusí fungovat správně, viz Nastavení PretyURLs.'
+);
 
-		'saveopt' 	=> 'Uložit nastavení',
+$lang ['admin'] ['plugin'] ['submenu'] ['prettyurls'] = 'PrettyURLs Config';
+$lang ['admin'] ['plugin'] ['prettyurls'] = array(
+	'head' => 'Nastavení PrettyURLs',
+	'htaccess' => '.htaccess',
+	'description' => 'Tímto editorem můžete editovat Váš soubor' . '<code><a class="hint" href="https://notepad-plus-plus.org/">.htaccess</a></code>.',
+	'cantsave' => 'Nemůžete editovat tento soubor, protože není <strong>zapisovatelný</strong>. Můžete nastavit povolení k zapisování nebo kopírovat a vložit do souboru a poté nahrát.',
+	'mode' => 'Režim',
+	'auto' => 'Automatický',
+	'autodescr' => 'se snaží vybrat nejlepší volbu',
+	'pathinfo' => 'Path Info',
+	'pathinfodescr' => 'např. /index.php/2011/01/01/hello-world/',
+	'httpget' => 'HTTP Get',
+	'httpgetdescr' => 'např. /?u=/2011/01/01/hello-world/',
+	'pretty' => 'Pretty',
+	'prettydescr' => 'např. /2011/01/01/hello-world/',
 
-		'submit'	=> 'Uložit .htaccess'
-	);
-	$lang['admin']['plugin']['prettyurls']['msgs'] = array(
-		1		=> '.htaccess úspěšně uložen',
-		-1		=> '.htaccess nemohl být uložen (nemáte oprávnění pro zapisování <code>'. BLOG_ROOT .'</code>)?',
+	'saveopt' => 'Uložit nastavení',
 
-		2		=> 'Možnosti úspěšně uloženy',
-		-2		=> 'Došlo k chybě při pokusu o uložení nastavení',
-	);
-	
+	'submit' => 'Uložit .htaccess'
+);
+$lang ['admin'] ['plugin'] ['prettyurls'] ['msgs'] = array(
+	1 => '.htaccess úspěšně uložen',
+	-1 => '.htaccess nemohl být uložen (nemáte oprávnění pro zapisování <code>' . BLOG_ROOT . '</code>)?',
+
+	2 => 'Možnosti úspěšně uloženy',
+	-2 => 'Došlo k chybě při pokusu o uložení nastavení'
+);
+
 ?>
