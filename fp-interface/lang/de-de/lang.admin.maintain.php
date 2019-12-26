ddc2af887bf68879353ae13b0238a4de56564867
azett <githubregistrierung@azett.com>
2019-12-26 19:07:56 +0100

    Fixed some grammar stuff in German language file.

--- fp-interface/lang/de-de/lang.admin.maintain.php
+++ fp-interface/lang/de-de/lang.admin.maintain.php
@@ -1,45 +1,39 @@
 <?php
-	
-	$lang['admin']['panel']['maintain'] = 'Verwaltung';
+$lang ['admin'] ['panel'] ['maintain'] = 'Verwaltung';
 
-	$lang['admin']['maintain']['default'] = array(
-		'head'		=> 'Flatpress Wartung',
-		'descr'		=> 'Dieses Menü bietet verschiedene Möglichkeiten für das Flatpress Blog, um '.
-					       'einige Dinge zu korrigieren oder einfach nur nach Updates zu suchen.',
-		'opt0'		=> '&laquo; Zurück zur Wartung',
-		'opt1'		=> 'Den Flatpress Index neu erstellen',
-		'opt2'		=> 'Den Theme- und Template Cache leeren',
-		'opt3'		=> 'Wiederherstellen der Dateizugriffsrechte',
-		'opt4'		=> 'Zeige PHP Informationen des Web Servers',
-		'opt5'		=> 'Prüfe auf neue Versionen',
+$lang ['admin'] ['maintain'] ['default'] = array(
+	'head' => 'Flatpress-Wartung',
+	'descr' => 'Dieses Menü bietet verschiedene Möglichkeiten für das Flatpress-Blog, um einige Dinge zu korrigieren oder einfach nur nach Updates zu suchen.',
+	'opt0' => '&laquo; Zurück zur Wartung',
+	'opt1' => 'Den Flatpress-Index neu erstellen',
+	'opt2' => 'Den Theme- und Template-Cache leeren',
+	'opt3' => 'Wiederherstellen der Dateizugriffsrechte',
+	'opt4' => 'Zeige PHP-Informationen des Webservers',
+	'opt5' => 'Prüfe auf neue Versionen',
 
-		'chmod_info'	=> "Die Dateizugriffsrechte <strong>konnten nicht</strong>
+	'chmod_info' => "Die Dateizugriffsrechte <strong>konnten nicht</strong>
 					auf die Default Werte von 0777 zurückgesetzt werden. 
-          Normalerweise kann man diesen Hinweis ignorieren.",
-		
-	);
-	
-	$lang['admin']['maintain']['default']['msgs'] = array(
-		1		=> 'Aktion ausgeführt.'
-	);
-	
-	$lang['admin']['maintain']['updates'] = array(
-		'head'	=> 'Updates',
-		'list'	=> '<ul>
-		<li>Du hast die FlatPress Version <big>%s</big></li>
-		<li>Die letzte stabile Flatpress Version ist <big><a href="%s">%s</a></big></li>
+          Normalerweise kann man diesen Hinweis ignorieren."
+);
+
+$lang ['admin'] ['maintain'] ['default'] ['msgs'] = array(
+	1 => 'Aktion ausgeführt.'
+);
+
+$lang ['admin'] ['maintain'] ['updates'] = array(
+	'head' => 'Updates',
+	'list' => '<ul>
+		<li>Du hast die FlatPress-Version <big>%s</big></li>
+		<li>Die letzte stabile Flatpress-Version ist <big><a href="%s">%s</a></big></li>
 		<li>Letzte Entwicklungsversion von Flatpress ist <big><a href="%s">%s</a></big></li>
 		</ul>',
-		'notice'=>'Hinweis:'
-		
-	);
-	
-	
-	
-	$lang['admin']['maintain']['updates']['msgs'] = array(
-		1		=> 'Es sind Updates verfügbar!',
-		2		=> 'Du benutzt bereits die aktuelle Version',
-		-1		=> 'Fehler: Es konnten keine Updateinformationen abgerufen werden'
-	);
+	'notice' => 'Hinweis:'
+);
+
+$lang ['admin'] ['maintain'] ['updates'] ['msgs'] = array(
+	1 => 'Es sind Updates verfügbar!',
+	2 => 'Du benutzt bereits die aktuelle Version',
+	-1 => 'Fehler: Es konnten keine Update-Informationen abgerufen werden'
+);
 
 ?>
