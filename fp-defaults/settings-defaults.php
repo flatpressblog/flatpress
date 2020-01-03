0cceecaa025d3c967c46d77cf63f55ba9a96c24b
azett <githubregistrierung@azett.com>
2020-01-03 18:19:54 +0100

    Leggero V2 is default theme (see #57)

--- fp-defaults/settings-defaults.php
+++ fp-defaults/settings-defaults.php
@@ -1,42 +1,34 @@
 <?php
-
-$fp_config = array (
-  'general' => 
-  array (
-    'www' => 'http://localhost',
-    'title' => 'FlatPress',
-    'subtitle' => 'My FlatPress blog',
-    'footer' => '',
-    'author' => 'FlatPress Team',
-    'email' => 'webmaster@localhost.com',
-    'startpage' => NULL,
-    'maxentries' => '5',
-    'notify' => true,
-    'theme' => 'leggero',
-    'style' => 'leggero',
-    'blogid' => 'fpdefid',
-    'charset' => 'utf-8',
-  ),
-  'locale' => 
-  array (
-    'timeoffset' => '2',
-    'timeformat' => '%H:%M:%S',
-    'dateformat' => '%A, %B %e, %Y',
-    'dateformatshort' => '%Y-%m-%d',
-    'charset' => 'utf-8',
-    'lang' => 'en-us',
-  ),
-  'plugins' =>
-  array (
-	'blockparser' =>
-	array (
-		'pages' =>
-			array(
-				'menu',
-				'about',
-			),
+$fp_config = array(
+	'general' => array(
+		'www' => 'http://localhost',
+		'title' => 'FlatPress',
+		'subtitle' => 'My FlatPress blog',
+		'footer' => '',
+		'author' => 'FlatPress Team',
+		'email' => 'webmaster@localhost.com',
+		'startpage' => NULL,
+		'maxentries' => '5',
+		'notify' => true,
+		'theme' => 'leggero',
+		'style' => 'leggero-v2',
+		'blogid' => 'fpdefid',
+		'charset' => 'utf-8'
 	),
-  ),
+	'locale' => array(
+		'timeoffset' => '2',
+		'timeformat' => '%H:%M:%S',
+		'dateformat' => '%A, %B %e, %Y',
+		'dateformatshort' => '%Y-%m-%d',
+		'charset' => 'utf-8',
+		'lang' => 'en-us'
+	),
+	'plugins' => array(
+		'blockparser' => array(
+			'pages' => array(
+				'menu',
+				'about'
+			)
+		)
+	)
 );
-
-?>
