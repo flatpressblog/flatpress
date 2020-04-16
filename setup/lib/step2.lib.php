e1c47d5b8cf7b3462d0103d5a6a7ec10dedc3c34
azett <githubregistrierung@azett.com>
2020-04-16 14:15:22 +0200

    Setup sets date and FP version for the freshly created static pages.

--- setup/lib/step2.lib.php
+++ setup/lib/step2.lib.php
@@ -1,34 +1,39 @@
 <?php
 
-	function check_step() {
-	
-		global $lang;
-		
-		$validate = validate();
-		
-		if ($validate) {
-		
-			$vl =& $lang['samplecontent'];
-			entry_save(array(
-				'subject' => $vl['entry']['subject'],
-				'content' => $vl['entry']['content']
-			));
-			
-			if (!static_exists('menu')) {	
-				static_save(array(
-					'subject' => $vl['menu']['subject'],
-					'content' => $vl['menu']['content']
-				), 'menu');
-			}
-			if (!static_exists('about')) {
-				static_save(array(
-					'subject' => $vl['about']['subject'],
-					'content' => $vl['about']['content']
-				), 'about');
-			}
+function check_step() {
+	global $lang;
+
+	$validate = validate();
+
+	if ($validate) {
+
+		$vl = & $lang ['samplecontent'];
+		entry_save(array(
+			'subject' => $vl ['entry'] ['subject'],
+			'content' => $vl ['entry'] ['content'],
+			'date' => time(),
+			'version' => system_ver()
+		));
+
+		if (!static_exists('menu')) {
+			static_save(array(
+				'subject' => $vl ['menu'] ['subject'],
+				'content' => $vl ['menu'] ['content'],
+				'date' => time(),
+				'version' => system_ver()
+			), 'menu');
 		}
-		
-		return $validate;
+		if (!static_exists('about')) {
+			static_save(array(
+				'subject' => $vl ['about'] ['subject'],
+				'content' => $vl ['about'] ['content'],
+				'date' => time(),
+				'version' => system_ver()
+			), 'about');
+		}
 	}
+
+	return $validate;
+}
 
 ?>
