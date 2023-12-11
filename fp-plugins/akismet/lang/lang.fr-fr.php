4b8ecce308f3d6ac6107dbb74cf35ef6b91e031d
Fraenkiman <frank.pcn@gmail.com>
2023-12-11 23:04:02 +0100

    code formatting

--- fp-plugins/akismet/lang/lang.fr-fr.php
+++ fp-plugins/akismet/lang/lang.fr-fr.php
@@ -7,9 +7,9 @@ $lang ['admin'] ['plugin'] ['submenu'] ['akismet'] = 'Configuration Akismet';
 
 $lang ['admin'] ['plugin'] ['akismet'] = array(
 	'head' => 'Configuration Akismet',
-	'description' => 'La plupart du temps, <a href="https://akismet.com/" target="_blank">Akismet</a> va grandement ' . // //
-		'ou compl&egrave;tement &eacute;liminer les commentaires ind&eacute;sirables sur votre blog. ' . // //
-		'Si vous n\'avez pas de compte sur akismet.com, vous pouvez vous en cr&eacute;er un sur ' . // //
+	'description' => 'La plupart du temps, <a href="https://akismet.com/" target="_blank">Akismet</a> va grandement ' . //
+		'ou compl&egrave;tement &eacute;liminer les commentaires ind&eacute;sirables sur votre blog. ' . //
+		'Si vous n\'avez pas de compte sur akismet.com, vous pouvez vous en cr&eacute;er un sur ' . //
 		'<a href="https://akismet.com/signup/" target="_blank">akismet.com/signup<a>.',
 	'apikey' => 'Clef API akismet.com',
 	'whatis' => '(<a href="https://akismet.com/support/getting-started/api-key/" target="_blank">Qu\'est-ce que c\'est?</a>)',
