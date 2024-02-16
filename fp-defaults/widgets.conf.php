86cb0930ca68b83197caf620aa7a47dab2003075
Fraenkiman <frank.pcn@gmail.com>
2024-02-17 00:31:24 +0100

    Reduce RSS and Atom Feed plugin to the widget
    - remove the RSS and Atom buttons in the header template
    - Activates the plugin by default and places the widget in the menu column

--- fp-defaults/widgets.conf.php
+++ fp-defaults/widgets.conf.php
@@ -1,45 +1,36 @@
 <?php
+$fp_widgets = array (
 
-  $fp_widgets = array (
-	
-	// to disable put // or # before the plugin name
-  	// remove it to enable :)
-  	
-  
-  'top' => 
-  array (
-    // (no widgets)
-  ),
+// to disable put // or # before the plugin name
+// remove it to enable :)
 
-  // Left side widgets. Put here blocks which will appear 
-  // on the left side
-  // (Theme dependant)
+'top' => array (
+	// (no widgets)
+),
 
-  'left' => 
-  array (
-    // (no widgets)
-  ),
+// Left side widgets. Put here blocks which will appear 
+// on the left side
+// (Theme dependant)
 
+'left' => array (
+	// (no widgets)
+),
 
-  // Right side widgets
-  'right' => 
-  array (
-    'adminarea',
-    'blockparser:menu',
-    'categories',
-    'archives',
-    //'calendar', // quite time consuming, not really recommended
-    'lastentries',
-    // 'lastcomments',
-    'searchbox',
-  ),
-  
-  'bottom' => 
-  array (
-    // (no widgets)
-  ),
+// Right side widgets
+'right' => array (
+	'adminarea',
+	'blockparser:menu',
+	'categories',
+	'archives',
+	//'calendar', // quite time consuming, not really recommended
+	'lastentries',
+	// 'lastcomments',
+	'searchbox',
+	'feed'
+),
 
-  
+'bottom' => array (
+	// (no widgets)
+),
 );
-
 ?>
