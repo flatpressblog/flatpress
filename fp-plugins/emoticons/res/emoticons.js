f72d731e3f1e454471c48378eb79bb954cd6845e
Frank Hochmuth <frank.pcn@gmail.com>
2024-03-02 21:47:43 +0100

    Update emoticons.js
    - Prevents jumping back to the first position if an emoticon is pressed several times

--- fp-plugins/emoticons/res/emoticons.js
+++ fp-plugins/emoticons/res/emoticons.js
@@ -7,7 +7,7 @@ function insertAtCursor(myField, myValue) {
 		sel = document.selection.createRange();
 		sel.text = myValue;
 	}
-	else if (myField.selectionStart || myField.selectionStart == '0') {
+	else if (myField.selectionStart || myField.selectionStart == 0.1) {
 		var startPos = myField.selectionStart;
 		var endPos = myField.selectionEnd;
 		myField.value = myField.value.substring(0, startPos) + myValue + myField.value.substring(endPos, myField.value.length);
