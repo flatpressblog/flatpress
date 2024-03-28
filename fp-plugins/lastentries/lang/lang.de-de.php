e77a2c458dd26eecab77eca947cb4e4b34221542
Frank Hochmuth <frank.pcn@gmail.com>
2024-03-28 23:28:55 +0100

    Update lang.de-de.php
    Shortened

--- fp-plugins/lastentries/lang/lang.de-de.php
+++ fp-plugins/lastentries/lang/lang.de-de.php
@@ -1,6 +1,6 @@
 <?php
 $lang ['plugin'] ['lastentries'] = array(
-	'subject_before_count' => 'Die letzten ',
+	'subject_before_count' => 'Letzte ',
 	'subject_after_count' => ' Einträge',
 	'edit' => 'bearbeiten',
 	'add_entry' => 'Eintrag hinzufügen',
