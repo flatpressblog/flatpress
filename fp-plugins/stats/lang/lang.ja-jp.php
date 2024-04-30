a731257aad01b30875d2066fe0f1bce445b8871f
Fraenkiman <frank.pcn@gmail.com>
2024-05-01 00:27:33 +0200

    Update lang.ja-jp.php

--- fp-plugins/stats/lang/lang.ja-jp.php
+++ fp-plugins/stats/lang/lang.ja-jp.php
@@ -3,15 +3,15 @@ $lang ['admin'] ['entry'] ['submenu'] ['stats'] = '統計情報';
 
 $lang ['admin'] ['entry'] ['stats'] = array(
 	'head' => '統計情報',
-	'entries' => 'エントリー',
+	'entries' => '記事',
 	'you_have' => '',
 	'entries_using' => '件、',
 	'characters_in' => '文字、',
-	'words' => '語。',
-	'total_disk_space_is' => '総収納容量は',
+	'words' => '語',
+	'total_disk_space_is' => '記憶域の使用量: ',
 	'comments' => 'コメント',
 	'comments_using' => '件',
-	'the' => 'コメント数が多かった',
-	'most_commented_entries' => '作品'
+	'the' => 'コメント多数の記事: ',
+	'most_commented_entries' => '件'
 );
 ?>
