497c26903786ff05a446acad2682ae8bbc6f57dd
eagleman <giacomomargarito@gmail.com>
2024-05-16 11:27:32 +0200

    Update lang.admin.php
    Updated the file

--- fp-interface/lang/it-it/lang.admin.php
+++ fp-interface/lang/it-it/lang.admin.php
@@ -1,34 +1,38 @@
 <?php
-
 	$lang['admin']['head'] = 'Pannello di controllo';
 	
 	$lang['admin']['panels'] = array(
 		'main'		=> 'Principale',
 		'config'	=> 'Opzioni',
-		//'users'		=> 'Users',
+		//'users'		=> 'Utenti',
 		'entry'		=> 'Articoli',
 		'static'	=> 'Pagine statiche',
 		'uploader'	=> 'Caricatore',
 		'widgets'	=> 'Widget',
-		//'add'		=> 'Add',
-		//'edit'		=> 'Edit/Delete',
+		//'add'		=> 'Aggiungi',
+		//'edit'		=> 'Modifica/Elimina',
 		'maintain'	=> 'Manutenzione',
 		'plugin'	=> 'Plugin',
 		'themes'	=> 'Temi',
-		//'updates'	=> 'Updates',
+		//'updates'	=> 'Aggiornamenti',
 	);
-	
-	
+		
 	$lang['admin']['general'] = array(
-		'id'		=> 'Id',	# for entry ids
-		'name'		=> 'Nome', 	# for file names
+		'id' => 'Id', // for entry ids
+	    'name' => 'Nome', // for file names
 		'title'		=> 'Titolo', # for entry/posts/static titles
 		'comments'	=> '#Commenti',
 		'author'	=> 'Autore',
 		'actdel'	=> 'Eliimina',
 		'actedit'	=> 'Modifica',
 		'actenable'	=> 'Abilita',
-		'actdisable'=> 'Disabilita'
-	);
+	    'actdisable' => 'Disabilita',
+	// New admin panel
+	'help_top'  => 'Guida',
+	'logout_top'    => 'Disconnettiti',
+	'close'		=> 'Chiudi',
+	'blog'		=> 'Blog',
+	'footer'	=> 'Questo blog è orgogliosamente realizzato con'
+);
 
 ?>
