9b0517bcd00cd4b080afa15698eceb75c16393e5
Fraenkiman <frank.pcn@gmail.com>
2024-08-20 21:02:16 +0200

    language-update

--- fp-plugins/gdprvideoembed/lang/lang.it-it.php
+++ fp-plugins/gdprvideoembed/lang/lang.it-it.php
@@ -6,23 +6,23 @@ $lang ['plugin'] ['gdprvideoembed'] = array(
 
 	'hint_youtube' => '<p><b>Nota:</b> Questo video incorporato è fornito da YouTube, LLC, 901 Cherry Ave, San Bruno, CA 94066, USA.<br>' .//
 		'Durante la riproduzione viene stabilita una connessione ai server di YouTube. Ciò indica a YouTube quali pagine si stanno visitando. ' . //
-		'Se l\'utente ha effettuato l\'accesso al proprio account YouTube, quest\'ultimo può assegnare il suo comportamento di navigazione all\'utente stesso. ' . //
+		'Se l´utente ha effettuato l´accesso al proprio account YouTube, quest´ultimo può assegnare il suo comportamento di navigazione all´utente stesso. ' . //
 		'È possibile evitare che ciò accada disconnettendosi preventivamente dal proprio account YouTube.</p>' . //
-		'<p>Quando si avvia un video di YouTube, il provider utilizza dei cookie che raccolgono informazioni sul comportamento dell\'utente.</p>' . //
+		'<p>Quando si avvia un video di YouTube, il provider utilizza dei cookie che raccolgono informazioni sul comportamento dell´utente.</p>' . //
 		'<p>Se avete disattivato la memorizzazione dei cookie per il programma Google Ads, non dovrete aspettarvi alcun cookie di questo tipo quando guardate i video di YouTube. ' . //
 		'Tuttavia, YouTube memorizza anche informazioni di utilizzo non personalizzate in altri cookie. ' . //
 		'Per evitare che ciò accada, è necessario bloccare la memorizzazione dei cookie nel proprio browser.</p>' . //
-		'<p>Ulteriori informazioni sulla protezione dei dati di „YouTube“ sono disponibili nell\'informativa sulla privacy del provider all\'indirizzo: ' . //
+		'<p>Ulteriori informazioni sulla protezione dei dati di „YouTube“ sono disponibili nell´informativa sulla privacy del provider all´indirizzo: ' . //
 		'<a href="https://policies.google.com/privacy?hl=it&gl=it" title="Vai a https://policies.google.com/privacy?hl=it&gl=it" rel="noopener" target="_blank">https://policies.google.com/privacy?hl=it&gl=it</a></p>',
 	'link_title_youtube' => 'Guarda il video su YouTube',
 
 	'head_vimeo' => 'Video Vimeo incorporato',
 	'hint_vimeo' => '<p><b>Nota:</b> Questo video incorporato è fornito da Vimeo, Inc, 555 West 18th Street, New York, New York 10011, USA.<br>' . //
 		'Durante la riproduzione viene stabilita una connessione ai server di Vimeo. Ciò indica a Vimeo quali pagine si stanno visitando. ' . //
-		'Se l\'utente ha effettuato l\'accesso al proprio account Vimeo, Vimeo può assegnare il suo comportamento di navigazione all\'utente stesso. ' . //
+		'Se l´utente ha effettuato l´accesso al proprio account Vimeo, Vimeo può assegnare il suo comportamento di navigazione all´utente stesso. ' . //
 		'È possibile evitare che ciò accada disconnettendosi preventivamente dal proprio account Vimeo.</p>' . //
-		'<p>Quando si avvia un video Vimeo, il provider utilizza dei cookie che raccolgono informazioni sul comportamento dell\'utente.</p>' . //
-		'<p>Ulteriori informazioni sulla protezione dei dati di “Vimeo” sono disponibili nell\'informativa sulla privacy del provider all\'indirizzo: ' . //
+		'<p>Quando si avvia un video Vimeo, il provider utilizza dei cookie che raccolgono informazioni sul comportamento dell´utente.</p>' . //
+		'<p>Ulteriori informazioni sulla protezione dei dati di “Vimeo” sono disponibili nell´informativa sulla privacy del provider all´indirizzo: ' . //
 		'<a href="https://vimeo.com/privacy" title="Vai a https://vimeo.com/privacy" rel="noopener" target="_blank">https://vimeo.com/privacy</a></p>',
 	'link_title_vimeo' => 'Guarda il video su Vimeo',
 
