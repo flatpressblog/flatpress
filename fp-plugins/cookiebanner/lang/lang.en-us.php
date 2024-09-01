0ec62b500421c76b8c55e1cea00342bbed45ac22
Frank Hochmuth <frank.pcn@gmail.com>
2024-09-01 03:06:48 +0200

    Update lang.en-us.php

--- fp-plugins/cookiebanner/lang/lang.en-us.php
+++ fp-plugins/cookiebanner/lang/lang.en-us.php
@@ -4,6 +4,6 @@ $lang ['plugin'] ['cookiebanner'] = array(
 	'bannertext' => 'I use <a href="https://en.wikipedia.org/wiki/HTTP_cookie" title="Wikipedia" target="_blank">cookies</a>, ' . //
 		'so that this website runs reliably and securely.',
 	'ok' => 'Okay',
-	'notice_text' => 'I process your data according to my <a href="' . $baseurl . 'static.php?page=dataprotection">privacy policy</a>.'
+	'notice_text' => 'I process your data according to my <a href="' . $baseurl . 'static.php?page=privacy-policy">privacy policy</a>.'
 );
 ?>
