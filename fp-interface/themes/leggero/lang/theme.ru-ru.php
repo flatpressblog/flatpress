f3a649562c95fdde3cb734eba09655a5bff9279f
Frank Hochmuth <frank.pcn@gmail.com>
2024-09-30 00:51:45 +0200

    Update theme.ru-ru.php
    typo

--- fp-interface/themes/leggero/lang/theme.ru-ru.php
+++ fp-interface/themes/leggero/lang/theme.ru-ru.php
@@ -2,7 +2,7 @@
 /**
  * Theme Name: Leggero
  * Theme URI: https://www.flatpress.org/
- * Description: TТема FlatPress по умолчанию, которая приносит глоток свежего мятного воздуха.<br><br>Эта тема поставляется с <a href="admin.php?p=themes&action=style">различными стилями</a> - попробуйте их :)
+ * Description: Tема FlatPress по умолчанию, которая приносит глоток свежего мятного воздуха.<br><br>Эта тема поставляется с <a href="admin.php?p=themes&action=style">различными стилями</a> - попробуйте их :)
  * Version: 0.705
  * Author: NoWhereMan и Drudo
  * Author URI: https://www.flatpress.org/
