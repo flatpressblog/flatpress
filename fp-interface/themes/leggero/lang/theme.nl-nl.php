feacf12e4ee5337721e7607d9f012328a645abcd
Frank Hochmuth <frank.pcn@gmail.com>
2024-09-30 00:52:49 +0200

    Update theme.nl-nl.php
    typo

--- fp-interface/themes/leggero/lang/theme.nl-nl.php
+++ fp-interface/themes/leggero/lang/theme.nl-nl.php
@@ -2,7 +2,7 @@
 /**
  * Theme Name: Leggero
  * Theme URI: https://www.flatpress.org/
- * Description: Het standaard FlatPress-thema dat een frisse wind laat waaien.<br><br>TDit thema wordt geleverd met <a href="admin.php?p=themes&action=style">verschillende stijlen</a> - probeer ze uit :)
+ * Description: Het standaard FlatPress-thema dat een frisse wind laat waaien.<br><br>Dit thema wordt geleverd met <a href="admin.php?p=themes&action=style">verschillende stijlen</a> - probeer ze uit :)
  * Version: 0.705
  * Author: NoWhereMan en Drudo
  * Author URI: https://www.flatpress.org/
