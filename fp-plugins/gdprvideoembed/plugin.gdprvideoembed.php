c2bc9794f8cceefa5dddba2d074c5d605c2fa6bc
Fraenkiman <frank.pcn@gmail.com>
2024-10-14 23:59:28 +0200

    GDPR video embed with external Facebook video link
    - The non-dynamic Java code has been outsourced to a file in favor of loading times.
    - An external link for Facebook video is now also displayed.

--- fp-plugins/gdprvideoembed/plugin.gdprvideoembed.php
+++ fp-plugins/gdprvideoembed/plugin.gdprvideoembed.php
@@ -23,79 +23,32 @@ function plugin_gdprvideoembed_head() {
 	echo '
 		<!-- BOF GDPR Video embed -->
 		<link rel="stylesheet" type="text/css" href="' . $plugindir . 'res/gdpr-video-embed.css.php">
+		<script nonce="' . $random_hex . '" src="' . $plugindir . 'res/gdpr-video-embed.js"></script>
 		<script nonce="' . $random_hex . '">
 			/**
-			 * GDPR-Video-Embed
+			 * GDPR-Video-Embed | dynamic part
 			 */
-			(function () {
-				// Config
-				var text = {
+			window.gdprConfig = {
+				text: {
 					youtube: \'<strong>' . $lang ['plugin'] ['gdprvideoembed'] ['head_youtube'] . '</strong>' . //
 						'<div>' . $lang ['plugin'] ['gdprvideoembed'] ['hint_youtube'] . '</div>' . //
 						'<a class="video-link" href="https://youtu.be/%id%" rel="noopener" target="_blank" ' . //
-							'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_youtube'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': https://youtu.be/%id%</a>' . //
+						'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_youtube'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': https://youtu.be/%id%</a>' . //
 						'<button title="' . $lang ['plugin'] ['gdprvideoembed'] ['button_title'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['button'] . '</button>\',
+
 					vimeo: \'<strong>' . $lang ['plugin'] ['gdprvideoembed'] ['head_vimeo'] . '</strong>' . //
 						'<div>' . $lang ['plugin'] ['gdprvideoembed'] ['hint_vimeo'] . '</div>' . //
 						'<a class="video-link" href="https://vimeo.com/%id%" rel="noopener" target="_blank" ' . //
-							'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_vimeo'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': https://vimeo.com/%id%</a>' . //
+						'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_vimeo'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': https://vimeo.com/%id%</a>' . //
 						'<button title="' . $lang ['plugin'] ['gdprvideoembed'] ['button_title'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['button'] . '</button>\',
+
 					facebook: \'<strong>' . $lang ['plugin'] ['gdprvideoembed'] ['head_facebook'] . '</strong>' . //
 						'<div>' . $lang ['plugin'] ['gdprvideoembed'] ['hint_facebook'] . '</div>' . //
-						//'<a class="video-link" href="https://www.facebook.com/%id%" rel="noopener" target="_blank" ' . //
-							//'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_facebook'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': https://www.facebook.com/%id%</a>' . //
+						'<a class="video-link" href="%video_url%" rel="noopener" target="_blank" ' . //
+						'title="' . $lang ['plugin'] ['gdprvideoembed'] ['link_title_facebook'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['link'] . ': %video_url%</a>' . //
 						'<button title="' . $lang ['plugin'] ['gdprvideoembed'] ['button_title'] . '">' . $lang ['plugin'] ['gdprvideoembed'] ['button'] . '</button>\'
-				};
-				window.video_iframes = [];
-				document.addEventListener("DOMContentLoaded", function () {
-					var video_frame, responsive_bbcode_video, video_platform, video_src, video_id, video_w, video_h;
-					for (var i = 0, max = window.frames.length - 1; i <= max; i += 1) {
-						video_frame = document.getElementsByTagName(\'iframe\')[0];
-						video_src = video_frame.src || video_frame.dataset.src;
-
-						// Only process video iframes [youtube|vimeo|facebook]
-						if (video_src.match(/youtube|vimeo|facebook/) == null) {
-							continue;
-						}
-
-						video_iframes.push(video_frame);
-						video_w = video_frame.getAttribute(\'width\');
-						video_h = video_frame.getAttribute(\'height\');
-						responsive_bbcode_video = document.createElement(\'article\');
-
-						// Prevent iframes from loading remote content
-						if (!!video_frame.src) {
-							if (typeof (window.frames[0].stop) === \'undefined\') {
-								setTimeout(function () {
-									window.frames[0].execCommand(\'Stop\');
-								}, 1000);
-							} else {
-								setTimeout(function () {
-									window.frames[0].stop();
-								}, 1000);
-							}
-						}
-						video_platform = video_src.match(/(youtube|vimeo|facebook)/)[0];
-						video_id = video_src.match(/(embed|video|videos)\/([^?\s]*)/)[2];
-						responsive_bbcode_video.setAttribute(\'class\', \'video-responsive_bbcode_video\');
-						responsive_bbcode_video.setAttribute(\'data-index\', i);
-						if (video_w && video_h) {
-							responsive_bbcode_video.setAttribute(\'style\', \'width: \' + video_w + \'px; height: \' + video_h + \'px\');
-						}
-						responsive_bbcode_video.innerHTML = text[video_platform].replace(/\%id\%/g, video_id);
-						video_frame.parentNode.replaceChild(responsive_bbcode_video, video_frame);
-						document.querySelectorAll(\'.video-responsive_bbcode_video button\')[i].addEventListener(\'click\', function () {
-							var video_frame = this.parentNode,
-								index = video_frame.dataset.index;
-							if (!!video_iframes[index].dataset.src) {
-								video_iframes[index].src = video_iframes[index].dataset.src;
-								video_iframes[index].removeAttribute(\'data-src\');
-							}
-							video_frame.parentNode.replaceChild(video_iframes[index], video_frame);
-						}, false);
-					}
-				});
-			})();
+				}
+			};
 		</script>
 		<!-- EOF GDPR Video embed -->
 	';
