de8639dc7155a51a589e14a5ae7aa8e5d4640578
Fraenkiman <frank.pcn@gmail.com>
2024-10-25 12:06:38 +0200

    Update to Smarty v4.5.4
    Full version comparison: https://github.com/smarty-php/smarty/compare/v4.3.1...v4.5.4
    - Fixed a code injection vulnerability in extends-tag. This addresses CVE-2024-35226.
    -  Fixed that using `count()` would trigger a deprecation notice.

--- fp-includes/smarty-4.5.4/libs/Smarty.class.php
+++ fp-includes/smarty-4.5.4/libs/Smarty.class.php
@@ -107,7 +107,7 @@ class Smarty extends Smarty_Internal_TemplateBase
     /**
      * smarty version
      */
-    const SMARTY_VERSION = '4.5.2';
+    const SMARTY_VERSION = '4.5.4';
     /**
      * define variable scopes
      */
