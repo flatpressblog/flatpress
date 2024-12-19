9b90f9889c750c0701dcd93d2e402a43a00c6836
Frank Hochmuth <frank.pcn@gmail.com>
2024-12-20 00:23:01 +0100

    Update main.php
    - Before setting authorizations, check whether the owner has write permissions

--- setup/main.php
+++ setup/main.php
@@ -1,18 +1,48 @@
 <?php
-//@error_reporting($_SERVER ['SERVER_NAME'] == "localhost" ? E_ALL : 0);
+if ($_SERVER ['SERVER_NAME'] === 'localhost') {
+	@ini_set('display_errors', 'on');
+	error_reporting(E_ALL);
+} else {
+	@ini_set('display_errors', 'off');
+	error_reporting(0);
+}
+
+function owner_has_write_permissions($path) {
+	if (!file_exists($path)) {
+		return false;
+	}
+	$is_writable = is_writable($path);
+	$perms = fileperms($path);
+	if ($perms === false) {
+		return false;
+	}
+	// -3, 1 owner, -1 others, -2 group/ others
+	$octal_perms = substr(sprintf('%o', $perms), -3, 1);
+	return $is_writable && (
+		// 2: Write permission for the group/others.
+		// 6: Read and write permission for the group/ others.
+		// 7: Full access (read, write, execute) for the group/others.
+		strpos($octal_perms, '2') !== false || strpos($octal_perms, '6') !== false || strpos($octal_perms, '7') !== false
+	);
+}
 
 // Changing file/directory permissions recursively
 function chmod_r($path, $filemode, $dirmode) {
 
 	if (!file_exists($path)) {
-		trigger_error('Path does not exist: ' . $path, E_USER_WARNING);
+		trigger_error('[FlatPress Setup] Path does not exist: ' . $path, E_USER_WARNING);
+		return false;
+	}
+
+	if (!owner_has_write_permissions($path)) {
+		trigger_error('[FlatPress Setup] The owner does not have write permission for the path: ' . $path, E_USER_WARNING);
 		return false;
 	}
 
 	if (is_dir($path)) {
 		if (!@chmod($path, $dirmode)) {
 			$dirmode_str = decoct($dirmode);
-			trigger_error('Failed to apply permissions ' . $dirmode_str . ' on directory: ' . $path, E_USER_WARNING);
+			trigger_error('[FlatPress Setup] Failed to apply permissions ' . $dirmode_str . ' on directory: ' . $path, E_USER_WARNING);
 			return false;
 		}
 
@@ -27,14 +57,14 @@ function chmod_r($path, $filemode, $dirmode) {
 	} elseif (is_file($path)) {
 		if (!@chmod($path, $filemode)) {
 			$filemode_str = decoct($filemode);
-			trigger_error('Failed to apply permissions ' . $filemode_str . ' on file: ' . $path, E_USER_WARNING);
+			trigger_error('[FlatPress Setup] Failed to apply permissions ' . $filemode_str . ' on file: ' . $path, E_USER_WARNING);
 			return false;
 		}
 	} elseif (is_link($path)) {
-		trigger_error('Skipping symlink: ' . $path, E_USER_WARNING);
+		trigger_error('[FlatPress Setup] Skipping symlink: ' . $path, E_USER_WARNING);
 		return false;
 	} else {
-		trigger_error('Unknown file type: ' . $path, E_USER_WARNING);
+		trigger_error('[FlatPress Setup] Unknown file type: ' . $path, E_USER_WARNING);
 		return false;
 	}
 
