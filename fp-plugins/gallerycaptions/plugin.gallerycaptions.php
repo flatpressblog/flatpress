f47078e5e8952b085c5d6604d338c0fc6ecad20b
Frank Hochmuth <frank.pcn@gmail.com>
2025-03-15 15:33:48 +0100

    Update plugin.gallerycaptions.php
    The Gallery captions plugin is only active if:
    - FlatPress equal to or greater than 1.3
    - the gallery_read_images function (core.gallery.php) exists
    - the AdminPanelAction class exists

--- fp-plugins/gallerycaptions/plugin.gallerycaptions.php
+++ fp-plugins/gallerycaptions/plugin.gallerycaptions.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: Gallery captions
  * Version: 1.0.1
@@ -9,17 +8,35 @@
  * Author URI: https://www.flatpress.org
  */
 
-// the minimum FlatPress version required
-const PLUGIN_GALLERYCAPTIONS_MINFPVERSION = '1.3';
+// The minimum FlatPress version required
+define('PLUGIN_GALLERYCAPTIONS_MINFPVERSION', '1.3');
 
-// include the plugin's PHP files
-if (class_exists('AdminPanelAction')) {
-	include_once dirname(__FILE__) . '/admin_uploader_gallerycaptions.class.php';
+// Cleanup and comparison of versions
+function checkFlatPressVersion($minVersion) {
 
-	// check if FP instance has the gallery functions
-	if (!function_exists('gallery_read_images')) {
+	if (!defined('SYSTEM_VER')) {
+		return false;
+	}
+
+	// SYSTEM_VER cleanup (e.g. "1.4-dev" -> "1.4")
+	preg_match('/^(\d+\.\d+)/', SYSTEM_VER, $matches);
+	if (!isset($matches [1])) {
+		// No suitable version found
+		return false;
+	}
+	$cleanVersion = $matches [1];
+
+	// Comparison: Current version >= minimum version?
+	return version_compare($cleanVersion, $minVersion, '>=');
+}
+
+// Check that the FlatPress version is correct, the class AdminPanelAction and that core.gallery.php is available
+if (checkFlatPressVersion(PLUGIN_GALLERYCAPTIONS_MINFPVERSION) && function_exists('gallery_read_images') && class_exists('AdminPanelAction')) {
+	require_once (dirname(__FILE__) . '/admin_uploader_gallerycaptions.class.php');
+} else {
+	if (class_exists('AdminPanelAction')) {
 		global $smarty;
 		$smarty->append('error', 'Sorry, you need FlatPress version ' . PLUGIN_GALLERYCAPTIONS_MINFPVERSION . ' or later in order to run the Gallery captions plugin.');
-		// FIXME: Deactivate plugin
 	}
 }
+?>
