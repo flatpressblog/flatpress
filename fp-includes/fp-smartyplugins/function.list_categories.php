fd34881f79277ab0b9be4bd8ad8aa0cd12fbf46e
Fraenkiman <frank.pcn@gmail.com>
2025-03-16 23:47:20 +0100

    PHPDoc and small improvements 001
    - Save string with fallback

--- fp-includes/fp-smartyplugins/function.list_categories.php
+++ fp-includes/fp-smartyplugins/function.list_categories.php
@@ -116,7 +116,7 @@ function do_print_categories_list(&$lines, &$indentstack, &$result, $params) {
 				$string .= 'checked="checked" ';
 			}
 
-			$string .= 'type="checkbox" />';
+			$string .= 'type="checkbox">';
 			$before = $string;
 		} elseif (isset($params ['type']) && $params ['type'] == 'radio') {
 			$string = '<label><input name="' . $catname . 'cats" type="radio" value="' . $vid . '"';
@@ -126,7 +126,7 @@ function do_print_categories_list(&$lines, &$indentstack, &$result, $params) {
 				$string .= 'checked="checked" ';
 			}
 
-			$string .= ' />';
+			$string .= '>';
 			$before = $string;
 		} elseif (isset($params ['type']) && $params ['type'] == 'linked') {
 			$before = '<a href="' . get_category_link($vid) . '">';
