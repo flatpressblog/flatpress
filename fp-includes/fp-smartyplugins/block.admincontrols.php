5cf29bc16061221ab8c90965aae5f4b82d3168cd
Fraenkiman <frank.pcn@gmail.com>
2025-03-17 01:04:55 +0100

    Update block.admincontrols.php
     If not logged in, empty string ''.

--- fp-includes/fp-smartyplugins/block.admincontrols.php
+++ fp-includes/fp-smartyplugins/block.admincontrols.php
@@ -2,16 +2,19 @@
 /**
  * Smarty {admincontrols}{/admincontrols} block plugin
  *
- * Type:     block function<br />
- * Name:     admincontrols<br />
+ * Type: block function<br />
+ * Name: admincontrols<br />
  * Purpose:  automatically show/hides admin controls<br />
  * @author NoWhereMan <monte at ohrt dot com>
- * @return string string $content if loggedin
+ * @param array $params Parameters passed to block
+ * @param string $content Content inside the block
+ * @param object $smarty Smarty template object
+ * @return string Content if logged in, otherwise empty string
  */
 function smarty_block_admincontrols($params, $content, &$smarty) {
 	if (user_loggedin()) {
 		return $content;
 	}
+	return '';
 }
-
 ?>
