c4795f134c849a3782ae8aa0a703fb96018ac37b
Fraenkiman <frank.pcn@gmail.com>
2025-04-10 21:46:14 +0200

    Update smarty_resource_recompiled.php

--- fp-includes/smarty-4.5.5/libs/sysplugins/smarty_resource_recompiled.php
+++ fp-includes/smarty-4.5.5/libs/sysplugins/smarty_resource_recompiled.php
@@ -74,8 +74,8 @@ abstract class Smarty_Resource_Recompiled extends Smarty_Resource
      */
     public function populateCompiledFilepath(Smarty_Template_Compiled $compiled, Smarty_Internal_Template $_template)
     {
-        $compiled->filepath = false;
-        $compiled->timestamp = false;
+        $compiled->filepath = '';
+        $compiled->timestamp = 0;
         $compiled->exists = false;
     }
 
