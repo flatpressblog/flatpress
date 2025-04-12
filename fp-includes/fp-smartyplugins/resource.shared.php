8b11eb986f9803f2f7ea2fe0489584b852b312ae
Fraenkiman <frank.pcn@gmail.com>
2025-04-12 21:04:34 +0200

    Update resource.shared.php

--- fp-includes/fp-smartyplugins/resource.shared.php
+++ fp-includes/fp-smartyplugins/resource.shared.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Resoure plugin that conveniently allows to include templates from the shared templates folder via {include file="shared:example.tpl"}
+ * Resource plugin that conveniently allows to include templates from the shared templates folder via {include file="shared:example.tpl"}
  *
  * @author FlatPress
  * @see https://www.smarty.net/docs/en/plugins.resources.tpl
@@ -9,6 +9,14 @@
 class Smarty_Resource_Shared extends Smarty_Resource_Custom {
 
 	/**
+	 * Fetch template source and modification time.
+	 *
+	 * @param string $name   Template name
+	 * @param string|null &$source Template source returned by reference (null if not found)
+	 * @param int|null    &$mtime  Modification time returned by reference (null if not found)
+	 *
+	 * @phpstan-param-out string|null $source
+	 * @phpstan-param-out int|null    $mtime
 	 *
 	 * {@inheritdoc}
 	 * @see Smarty_Resource_Custom::fetch()
@@ -23,3 +31,4 @@ class Smarty_Resource_Shared extends Smarty_Resource_Custom {
 	}
 
 }
+?>
