dc531b33667dacc5641182ed8678b2e19fc545c6
Frank Hochmuth <frank.pcn@gmail.com>
2025-04-15 14:45:46 +0200

    Update theme.tr-tr.php

--- fp-interface/themes/leggero/lang/theme.tr-tr.php
+++ fp-interface/themes/leggero/lang/theme.tr-tr.php
@@ -4,7 +4,7 @@
  * Theme URI: https://www.flatpress.org/
  * Description: Taze nane havası getiren standart FlatPress teması.<br><br>Bu tema <a href=“admin.php?p=themes&action=style”>farklı stillerle</a> birlikte gelir - deneyin :)
  * Version: 0.705
- * Author: NoWhereMan und Drudo
+ * Author: NoWhereMan ve Drudo
  * Author URI: https://www.flatpress.org/
  */
 ?>
