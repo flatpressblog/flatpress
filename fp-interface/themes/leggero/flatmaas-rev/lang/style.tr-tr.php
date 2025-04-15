32d39c44d1f56119e0d0766912b3a8437af4bbe9
Fraenkiman <frank.pcn@gmail.com>
2025-04-15 14:35:51 +0200

    Turkish description of the theme and styles
    - Supplement to #576

---
+++ fp-interface/themes/leggero/flatmaas-rev/lang/style.tr-tr.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Style Name: FlatMaas Revisited
+ * Style URI: https://www.flatpress.org/
+ * Description: FlatPress'in eski standardı, şimdi Leggero tarzında.
+ * Version: 0.705
+ * Author: Drudo
+ * Author URI: https://www.flatpress.org/
+ */
+?>
