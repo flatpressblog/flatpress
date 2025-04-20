7dc879368c3dda87454f273aa4d5bd720116397d
Fraenkiman <frank.pcn@gmail.com>
2025-04-20 13:10:55 +0200

    Update core.theme.php

--- fp-includes/core/core.theme.php
+++ fp-includes/core/core.theme.php
@@ -11,7 +11,7 @@ function theme_loadsettings() {
 		// theme website
 		'www' => 'https://www.flatpress.org',
 		// fp version
-		'version' => null,
+		'version' => -1,
 		// default style (must be in res/ dir
 		'default_style' => null,
 		'style' => array(
@@ -46,7 +46,8 @@ function theme_loadsettings() {
 	}
 
 	if (!defined('THEME_LEGACY_MODE')) {
-		if ($theme ['version'] !== null && is_numeric($theme ['version']) && $theme ['version'] < 0.702) {
+		/** @phpstan-ignore-next-line */
+		if ($theme ['version'] < 0.702) {
 			define('THEME_LEGACY_MODE', true);
 			theme_register_default_widgetsets();
 		} else {
