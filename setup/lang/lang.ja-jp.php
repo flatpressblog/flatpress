13e9abc13ec06b90e74487e8fc3b1963053335bf
Fraenkiman <frank.pcn@gmail.com>
2025-05-30 19:07:53 +0200

    Update of the setup in Japanese language

--- setup/lang/lang.ja-jp.php
+++ setup/lang/lang.ja-jp.php
@@ -105,7 +105,7 @@ $lang ['samplecontent'] ['menu'] ['content'] = '[list]
 [/list]';
 
 $lang ['samplecontent'] ['entry'] ['subject'] = 'FlatPressへ ようこそ!';
-$lang ['samplecontent'] ['entry'] ['content'] = 'これは[url=https://www.flatpress.org target=_blank rel=external]FlatPress[/url]の特徴をいくつかご紹介するサンプル記事です。
+$lang ['samplecontent'] ['entry'] ['content'] = 'これは [url=https://www.flatpress.org target=_blank rel=external]FlatPress[/url] の特徴をいくつかご紹介するサンプル記事です。
 
 ［more］タグを使用すると、抜粋記事から記事全文への橋渡しをする［続きを読む...］リンクが作成されます。
 
@@ -114,44 +114,44 @@ $lang ['samplecontent'] ['entry'] ['content'] = 'これは[url=https://www.flatp
 
 [h4]装飾と書式[/h4]
 
-記事への装飾や書式の指定には[url=https://wiki.flatpress.org/doc:plugins:bbcode target=_blank rel=external]BBcode[/url] (bulletin board code)がデフォルトで用意されています。BBCodeなら簡単に記事へ装飾できます。よくある装飾用タグが用意されています。例えば［b］タグで [b]ボールド[/b] (html: strong)、［i］タグで [i]イタリック[/i] (html: i)、などです。
+記事本文への装飾や書式の指定には [url=https://wiki.flatpress.org/doc:plugins:bbcode target=_blank rel=external]BBcode[/url] (bulletin board code) がデフォルトで用意されています。BBCodeなら簡単に記事へ装飾できます。よくある装飾用タグが用意されています。例えば［b］タグで [b]ボールド[/b](太字)、［i］タグで [i]イタリック[/i](斜体)、などです。
 
-[quote]お気に入りの引用を表示するための[b]quote[/b] タグもあります。[/quote]
+[quote]お気に入りの引用を表示するための[b]［quote］[/b]タグもあります。[/quote]
 
-[code]また、［code］タグではスニペットが等幅形式で表示されます。
-   コンテンツのインデントも
-      対応しています。[/code]
+[code]また、［code］タグでは 整形済みテキストが等幅形式で表示されます。
+   スペース(空白)を使ったコンテンツのインデントだってほら...
+      対応しています! :)[/code]
 
-［img］タグと［url］タグには特別なオプションもあります。詳しくは、[url=https://wiki.flatpress.org/doc:plugins:bbcode target=_blank rel=external]FlatPress official website[/url]をご覧ください。
+［img］タグと［url］タグには特別なオプションもあります。詳しくは、[url=https://wiki.flatpress.org/doc:plugins:bbcode target=_blank rel=external]FlatPress official website[/url] をご覧ください。
 
 
 [h4]ブログ記事と固定ページ[/h4]
 
-お読みいただいてるのはブログ記事ですが、[url=static.php?page=about]「自己紹介」[/url] は [b]固定ページ[/b]です。固定ページにはコメントができません。またブログ記事と同時には表示されません。
+いまお読みいただいてるサンプルはブログ記事ですが、[url=static.php?page=about]「自己紹介」[/url] は [b]固定ページ[/b]です。固定ページにはコメントができません。またブログ記事と同時には表示されません。
 
-固定ページは広報ページに向いています。 また[b]サイトのトップ(優先表示する)ページ[/b]に指定することもできます。ひいてはブログサイトでないサイト運営も可能ということです。固定ページを「サイトのトップ(優先表示する)ページ」に指定するには [url=admin.php]管理者ページ[/url] の [b]設定[/b] を開きます。
+固定ページは広報や告知などに向いています。 また [b]サイトのトップ(優先表示する)ページ[/b] に指定することもできます。ひいてはブログサイトでないサイト運営も可能ということです。固定ページを「サイトのトップ(優先表示する)ページ」に指定するには [url=admin.php]管理者用ページ[/url] の [b]設定[/b] を開きます。
 
 
 [h4]プラグイン[/h4]
 
-FlatPressは非常に高いカスタマイズ性があり、 [url=https://wiki.flatpress.org/doc:plugins:standard target=_blank rel=external]プラグイン[/url] で機能を拡張することができます。ちなみに BBCode自身もプラグイン機能です。
+FlatPressは非常に高いカスタマイズ性があり、[url=https://wiki.flatpress.org/doc:plugins:standard target=_blank rel=external]プラグイン[/url] で機能を拡張することができます。ちなみに BBCode自身もプラグインによる機能です。
 
 FlatPressの隠された珠玉の機能を紹介するためサンプルページを作りました :) 
 あなたの編集を待っている [b]固定ページ[/b] が2つあります:
 [list]
 [*][url=static.php?page=about]自己紹介[/url]
-[*][url=static.php?page=menu]メニュー[/url] (ページ内のリンクがサイドバーにも反映されることにご注目ください。これぞ[b]BlockParserウィジェット[/b]の魔法なのです。このウィジェットやその他の詳細については[url=https://wiki.flatpress.org/doc:faq target=_blank rel=external]FAQ[/url]を参照してください!)
+[*][url=static.php?page=menu]メニュー[/url] (ページ内のリンクがサイドバーにも反映されることにご注目ください。これぞ[b]BlockParserウィジェットプラグイン[/b]の魔法なのです。このウィジェットやその他の詳細については [url=https://wiki.flatpress.org/doc:faq target=_blank rel=external]FAQ[/url] を参照してください!)
 [/list]
 
-[b]PhotoSwipeプラグイン[/b]を使用すると、float="left"またはfloat="right"で揃えられた単一の画像として、いとも簡単に画像を配置できます。
+[b]PhotoSwipeプラグイン[/b] を使用すると、float="left" または float="right" で揃えられた単一の画像として、いとも簡単に画像を配置できます。
 ［gallery］タグを使用すると、ギャラリー単位で配置することもできます。そのやり方がいかに簡単かは、[url=https://wiki.flatpress.org/doc:plugins:photoswipe target=_blank rel=external]こちらで学べます[/url]。
 
 
 [h4]ウィジェット[/h4]
 
-サイドバーには固定された部品が1つもありません。この記事周辺のサイドバー上にあるすべての部品は、すべて自由に配置でき、そのほとんどはカスタマイズ可能です。その機能によっては、管理者用ページに設定用パネルが用意されているものもあります。
+サイドバーには固定された部品が1つもありません。この記事欄周辺のサイドバー上にある部品はすべて、自由に配置でき、そのほとんどはカスタマイズ可能です。その機能によっては、管理者用ページに設定用パネルが用意されているものもあります。
 
-これらの部品は[b]ウィジェット[/b]と呼ばれます。ウィジェットの詳細やナイスな機能を得るための[url=https://wiki.flatpress.org/doc:tips:widgets target=_blank rel=external]ヒント[/url]は、[url=https://wiki.flatpress.org/ target=_blank rel=external]wiki[/url]からご覧ください。
+これらの部品は[b]ウィジェット[/b]と呼ばれます。ウィジェットの詳細やナイスな機能を得るための [url=https://wiki.flatpress.org/doc:tips:widgets target=_blank rel=external]ヒント[/url] は、[url=https://wiki.flatpress.org/ target=_blank rel=external]wiki[/url] からご覧ください。
 
 
 [h4]テーマ[/h4]
@@ -165,30 +165,30 @@ FlatPress-Leggeroテーマでは、クラシックからモダンまで3つの�
 もっと知りたいですか?
 
 [list]
-[*]FlatPressの世界で何が起こっているかを知るには、[url=https://www.flatpress.org/?x target=_blank rel=external]公式ブログ[/url]をフォローしてください。
-[*]サポートや雑談については[url=https://forum.flatpress.org/ target=_blank rel=external]フォーラム[/url]にアクセスしてください。
-[*][url=https://wiki.flatpress.org/res:themes target=_blank rel=external]他のユーザから投稿[/url]された[b]素晴らしいテーマ[/b]を入手しましょう!
-[*][url=https://wiki.flatpress.org/res:plugins target=_blank rel=external]プラグイン[/url]をチェックしてください。
-[*]あなたの言語の[url=https://wiki.flatpress.org/res:language target=_blank rel=external]翻訳パック[/url]を入手しましょう。
-[*][url=https://fosstodon.org/@flatpress target=_blank rel=external]Mastodon[/url]でもFlatPressをフォローできます。
+[*]FlatPressの世界で何が起こっているかを知るには、[url=https://www.flatpress.org/?x target=_blank rel=external]公式ブログ[/url] をフォローしてください。
+[*]サポートや雑談については [url=https://forum.flatpress.org/ target=_blank rel=external]フォーラム[/url] にアクセスしてください。
+[*]他のユーザから投稿された [url=https://wiki.flatpress.org/res:themes target=_blank rel=external]素晴らしいテーマ[/url] を入手しましょう!
+[*][url=https://wiki.flatpress.org/res:plugins target=_blank rel=external]プラグイン[/url] をチェックしてください。
+[*]あなたの言語の [url=https://wiki.flatpress.org/res:language target=_blank rel=external]翻訳パック[/url] を入手しましょう。
+[*][url=https://fosstodon.org/@flatpress target=_blank rel=external]Mastodon[/url] でもFlatPressをフォローできます。
 [/list]
 
 
 [h4]お手伝いできますか?[/h4]
 
 [list]
-[*][url=https://www.flatpress.org/home/static.php?page=donate target=_blank rel=external]少額寄付[/url]でプロジェクトを支援してください。
-[*]バグ報告や改善提案は、[url=https://www.flatpress.org/contact/ target=_blank rel=external]お問い合わせ[/url]まで。
-[*][url=https://github.com/flatpressblog/flatpress target=_blank rel=external]GitHub[/url]で FlatPressの開発に貢献してください。
-[*]FlatPressまたはドキュメントを[url=https://wiki.flatpress.org/res:language target=_blank rel=external]あなたの言語[/url]に翻訳してください。
-[*][url=https://forum.flatpress.org/ target=_blank rel=external]フォーラム[/url]で知識を共有し、他の FlatPressユーザとつながりましょう。
+[*][url=https://www.flatpress.org/home/static.php?page=donate target=_blank rel=external]少額寄付[/url] でプロジェクトを支援してください。
+[*]バグ報告や改善提案は、[url=https://www.flatpress.org/contact/ target=_blank rel=external]お問い合わせ[/url] まで。
+[*][url=https://github.com/flatpressblog/flatpress target=_blank rel=external]GitHub[/url] で FlatPressの開発に貢献してください。
+[*]FlatPressまたはドキュメントを [url=https://wiki.flatpress.org/res:language target=_blank rel=external]あなたの言語[/url] に翻訳してください。
+[*][url=https://forum.flatpress.org/ target=_blank rel=external]フォーラム[/url] で知識を共有し、他の FlatPressユーザとつながりましょう。
 [*]広めてください! :)
 [/list]
 
 
 [h4]ではさっそく何をしましょうか?[/h4]
 
-さっそく[url=admin.php]管理者用ページ[/url]に[url=login.php]ログイン[/url]して投稿してみましょう!
+さっそく [url=admin.php]管理者用ページ[/url] に [url=login.php]ログイン[/url] して投稿してみましょう!
 
 さあ、存分にお楽しみあれ! :)
 
