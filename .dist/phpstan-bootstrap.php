53f90e0d22aa4a0f7af8bb39c059feca5dfbc500
Frank Hochmuth <frank.pcn@gmail.com>
2025-08-13 15:20:28 +0200

    Update phpstan-bootstrap.php

--- .dist/phpstan-bootstrap.php
+++ .dist/phpstan-bootstrap.php
@@ -1,6 +1,5 @@
 
 <?php
-declare(strict_types=1);
 /**
  * PHPStan Bootstrap for FlatPress without Composer + Smarty 5.5.1
  * No runtime side effects: no sessions, no system_init(), no plugin load.
