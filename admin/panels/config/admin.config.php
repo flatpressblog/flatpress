1f7be0da87cf6d24561443285ec1eab8ead3daf0
Fraenkiman <frank.pcn@gmail.com>
2025-09-19 21:41:39 +0200

    Update admin.config.php

--- admin/panels/config/admin.config.php
+++ admin/panels/config/admin.config.php
@@ -251,8 +251,8 @@ class admin_config_default extends AdminPanelActionValidated {
 			$this->delay_logout(5);
 		}
 
-		// Call main() to render updated config without reload
-		return $this->main();
+		// Call setup() to render updated config without reload
+		return $this->setup();
 	}
 
 	function onerror() {
