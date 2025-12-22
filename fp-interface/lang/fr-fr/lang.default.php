bab31b1f3db534548ab40a5fe5ac4272e1940632
Frank Hochmuth <frank.pcn@gmail.com>
2025-12-22 14:29:48 +0100

    Update lang.default.php

--- fp-interface/lang/fr-fr/lang.default.php
+++ fp-interface/lang/fr-fr/lang.default.php
@@ -26,7 +26,7 @@ $lang ['main'] = array(
 	'atom' => 'S’abonner au flux Atom'
 );
 
-$lang['search'] = array(
+$lang ['search'] = array(
 	'head' => 'Rechercher',
 	'fset1' => 'Entrer un critère de recherche',
 	'keywords' => 'Mots-clés',
@@ -52,44 +52,44 @@ $lang['search'] = array(
 	'searchag' => 'Nouvelle recherche'
 );
 
-$lang['search']['error'] = array(
+$lang ['search'] ['error'] = array(
 	'keywords' => 'Vous devez entrer au moins un mot-clé.'
 );
 
-$lang['staticauthor'] = array(
+$lang ['staticauthor'] = array(
 	'published_by' => 'Publié par',
 	'on' => 'le'
 );
 
-$lang['entryauthor'] = array(
+$lang ['entryauthor'] = array(
 	'posted_by' => 'Posté par',
 	'at' => 'à'
 );
 
-$lang['entry'] = array();
-$lang['entry']['flags'] = array();
+$lang ['entry'] = array();
+$lang ['entry'] ['flags'] = array();
 
-$lang['entry']['flags']['long'] = array(
+$lang ['entry'] ['flags'] ['long'] = array(
 	'draft' => '<strong>Brouillon</strong> : non publié pour l’instant',
 	'commslock' => '<strong>Commentaires désactivés</strong> : les commentaires sont fermés pour ce billet'
 );
 
-$lang['entry']['flags']['short'] = array(
+$lang ['entry'] ['flags'] ['short'] = array(
 	'draft' => 'Brouillon',
 	'commslock' => 'Commentaires désactivés'
 );
 
-$lang['entry']['categories'] = array(
+$lang ['entry'] ['categories'] = array(
 	'unfiled' => 'Non classé'
 );
 
-$lang['404error'] = array(
+$lang ['404error'] = array(
 	'subject' => 'Page non trouvée',
 	'content' => '<p>Désolé, la page demandée est introuvable !</p>'
 );
 
 // Connexion
-$lang['login'] = array(
+$lang ['login'] = array(
 	'head' => 'Connexion',
 	'fieldset1' => 'Entrez vos identifiants',
 	'user' => 'Nom d’utilisateur :',
@@ -99,7 +99,7 @@ $lang['login'] = array(
 	'forgot' => 'Mot de passe oublié ?'
 );
 
-$lang['login']['success'] = array(
+$lang ['login'] ['success'] = array(
 	'success' => 'Vous êtes maintenant connecté.',
 	'logout' => 'Vous êtes maintenant déconnecté.',
 	'redirect' => 'Vous allez être redirigé dans 5 secondes.',
@@ -108,14 +108,14 @@ $lang['login']['success'] = array(
 	'opt3' => 'Créer un nouveau billet'
 );
 
-$lang['login']['error'] = array(
+$lang ['login'] ['error'] = array(
 	'user' => 'Vous devez entrer un nom d’utilisateur.',
 	'pass' => 'Vous devez entrer un mot de passe.',
 	'match' => 'Nom d’utilisateur ou mot de passe incorrect.',
 	'timeout' => 'Veuillez attendre 30 secondes avant de réessayer.'
 );
 
-$lang['comments'] = array(
+$lang ['comments'] = array(
 	'head' => 'Ajouter un commentaire',
 	'descr' => 'Remplissez le formulaire ci-dessous pour ajouter un commentaire.',
 	'fieldset1' => 'Informations utilisateur',
@@ -133,18 +133,18 @@ $lang['comments'] = array(
 	'commslock' => 'Les commentaires sont fermés pour ce billet.'
 );
 
-$lang['comments']['error'] = array(
+$lang ['comments'] ['error'] = array(
 	'name' => 'Vous devez entrer un nom.',
 	'email' => 'Vous devez entrer une adresse électronique valide.',
 	'www' => 'Vous devez entrer une URL valide.',
 	'comment' => 'Vous devez entrer un commentaire.'
 );
 
-$lang['postviews'] = array(
+$lang ['postviews'] = array(
 	'views' => 'Vues'
 );
 
-$lang['date']['month'] = array(
+$lang ['date'] ['month'] = array(
 	'Janvier',
 	'Février',
 	'Mars',
@@ -159,7 +159,7 @@ $lang['date']['month'] = array(
 	'Décembre'
 );
 
-$lang['date']['month_abbr'] = array(
+$lang ['date'] ['month_abbr'] = array(
 	'Jan',
 	'Fév',
 	'Mar',
@@ -174,7 +174,7 @@ $lang['date']['month_abbr'] = array(
 	'Déc'
 );
 
-$lang['date']['weekday'] = array(
+$lang ['date'] ['weekday'] = array(
 	'Dimanche',
 	'Lundi',
 	'Mardi',
@@ -184,7 +184,7 @@ $lang['date']['weekday'] = array(
 	'Samedi'
 );
 
-$lang['date']['weekday_abbr'] = array(
+$lang ['date'] ['weekday_abbr'] = array(
 	'Dim',
 	'Lun',
 	'Mar',
