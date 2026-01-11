1695cf2713b3cc195ee2abf6a2c79eff7a50209a
Fraenkiman <frank.pcn@gmail.com>
2026-01-11 18:47:38 +0100

    FlatPress app
    - Optional FlatPress app installation for Android/iOS from shared web hosts stabilized.

--- fp-plugins/favicon/imgs/browserconfig.xml.php
+++ fp-plugins/favicon/imgs/browserconfig.xml.php
@@ -1,22 +1,60 @@
-<?php header('Content-Type: text/xml; charset=utf-8');
+<?php
+declare(strict_types=1);
 @error_reporting(0);
 
-require_once('../../../defaults.php');
-$blogBaseUrl = BLOG_BASEURL;
+header('Content-Type: application/xml; charset=utf-8');
+header('X-Content-Type-Options: nosniff');
 
-// Indicates the version of the symbol. Increase it by one when you change the image ($v = '?v=3', $v = '?v=4', etc.).
-// The browser will then immediately display the latest version.
-$v = '?v=2';
+require_once dirname(__FILE__, 4) . '/defaults.php';
+require_once dirname(__FILE__, 4) . '/fp-includes/core/core.system.php';
+
+$baseUrl = BLOG_BASEURL;
+if (!is_string($baseUrl) || $baseUrl === '') {
+	$baseUrl = '';
+}
+$baseUrl = rtrim($baseUrl, "/") . "/";
+
+$pluginsDir = defined('PLUGINS_DIR') ? (string)PLUGINS_DIR : 'fp-plugins/';
+$pluginsDir = trim($pluginsDir, "/\\") . '/';
+$pluginRel = $pluginsDir . 'favicon/imgs/';
+
+$siteBase = $baseUrl;
+if (substr($baseUrl, -strlen($pluginRel)) === $pluginRel) {
+	$siteBase = substr($baseUrl, 0, -strlen($pluginRel));
+}
+$siteBase = rtrim($siteBase, "/") . "/";
+
+$imgUrlBase = $siteBase . $pluginRel;
+$imgFsBase = rtrim(ABS_PATH, "/\\") . '/' . PLUGINS_DIR . 'favicon/imgs/';
+
+$assetVer = function (string $filename) use ($imgFsBase): string {
+	$file = $imgFsBase . $filename;
+	if (is_file($file)) {
+		$mtime = @filemtime($file);
+		if (is_int($mtime) && $mtime > 0) {
+			return '?v=' . rawurlencode((string)$mtime);
+		}
+	}
+	if (defined('SYSTEM_VER')) {
+		return '?v=' . rawurlencode((string)SYSTEM_VER);
+	}
+	return '';
+};
+
+$e = function (string $s): string {
+	return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+};
+
+echo '<?xml version="1.0" encoding="utf-8"?>' . "\n";
+echo '<browserconfig>' . "\n";
+echo "	<msapplication>\n";
+echo "		<tile>\n";
+echo "			<square70x70logo src=\"" . $e($imgUrlBase . 'mstile-70x70.png' . $assetVer('mstile-70x70.png')) . "\"/>\n";
+echo "			<square150x150logo src=\"" . $e($imgUrlBase . 'mstile-150x150.png' . $assetVer('mstile-150x150.png')) . "\"/>\n";
+echo "			<square310x310logo src=\"" . $e($imgUrlBase . 'mstile-310x310.png' . $assetVer('mstile-310x310.png')) . "\"/>\n";
+echo "			<wide310x150logo src=\"" . $e($imgUrlBase . 'mstile-310x150.png' . $assetVer('mstile-310x150.png')) . "\"/>\n";
+echo "			<TileColor>#b77b7b</TileColor>\n";
+echo "		</tile>\n";
+echo "	</msapplication>\n";
+echo '</browserconfig>' . "\n";
 ?>
-<?xml version="1.0" encoding="utf-8"?>
-<browserconfig>
-	<msapplication>
-		<tile>
-			<square70x70logo src="<?php echo $blogBaseUrl;?>mstile-70x70.png<?php echo $v;?>"/>
-			<square150x150logo src="<?php echo $blogBaseUrl;?>mstile-150x150.png<?php echo $v;?>"/>
-			<square310x310logo src="<?php echo $blogBaseUrl;?>mstile-310x310.png<?php echo $v;?>"/>
-			<wide310x150logo src="<?php echo $blogBaseUrl;?>mstile-310x150.png<?php echo $v;?>"/>
-			<TileColor>#b77b7b</TileColor>
-		</tile>
-	</msapplication>
-</browserconfig>
--- fp-plugins/favicon/imgs/site.webmanifest.json.php
+++ fp-plugins/favicon/imgs/site.webmanifest.json.php
@@ -1,26 +1,115 @@
-<?php header('Content-Type: text/json; charset=utf-8');
-@error_reporting(0);
+<?php
+declare(strict_types=1);
 
-require_once('../../../defaults.php');
-$blogBaseUrl = BLOG_BASEURL;
+header('Content-Type: application/manifest+json; charset=utf-8');
+header('X-Content-Type-Options: nosniff');
 
-// Indicates the version of the symbol. Increase it by one when you change the image ($v = '?v=3', $v = '?v=4', etc.).
-// The browser will then immediately display the latest version.
-$v = '?v=2';
-?>
-{
-	"icons": [
-		{
-			"src": "<?php echo $blogBaseUrl;?>android-chrome-192x192.png<?php echo $v;?>",
-			"sizes": "192x192",
-			"type": "image/png"
-		},
-		{
-			"src": "<?php echo $blogBaseUrl;?>android-chrome-256x256.png<?php echo $v;?>",
-			"sizes": "256x256",
-			"type": "image/png"
-		}
-	],
-	"theme_color": "#b77b7b",
-	"background_color": "#b77b7b"
+require_once dirname(__FILE__, 4) . '/defaults.php';
+require_once dirname(__FILE__, 4) . '/fp-includes/core/core.system.php';
+
+$fp_config = array();
+$settingsFile = rtrim(ABS_PATH, "/\\") . '/' . FP_CONTENT . 'config/settings.conf.php';
+if (is_file($settingsFile)) {
+	include $settingsFile;
 }
+
+$title = $fp_config ['general'] ['title'] ?? '';
+$subtitle = $fp_config ['general'] ['subtitle'] ?? '';
+
+$host = parse_url(BLOG_BASEURL, PHP_URL_HOST);
+if (!is_string($host) || $host === '') {
+	$host = 'FlatPress';
+}
+
+if (!is_string($title) || $title === '') {
+	$title = $host;
+}
+
+$shortName = $title;
+if (!is_string($subtitle)) {
+	$subtitle = '';
+}
+
+$description = ($subtitle !== '') ? $subtitle : $title;
+
+$baseUrl = BLOG_BASEURL;
+if (!is_string($baseUrl) || $baseUrl === '') {
+	$baseUrl = '';
+}
+$baseUrl = rtrim($baseUrl, "/") . "/";
+
+$pluginsDir = defined('PLUGINS_DIR') ? (string)PLUGINS_DIR : 'fp-plugins/';
+$pluginsDir = trim($pluginsDir, "/\\") . '/';
+$pluginRel = $pluginsDir . 'favicon/imgs/';
+
+$siteBase = $baseUrl;
+if (substr($baseUrl, -strlen($pluginRel)) === $pluginRel) {
+	$siteBase = substr($baseUrl, 0, -strlen($pluginRel));
+}
+$siteBase = rtrim($siteBase, "/") . "/";
+
+$startUrl = $siteBase;
+$scope = $siteBase;
+
+$imgUrlBase = $siteBase . $pluginRel;
+$imgFsBase = rtrim (ABS_PATH, "/\\") . '/' . PLUGINS_DIR . 'favicon/imgs/';
+
+$assetVer = function (string $filename) use ($imgFsBase): string {
+	$file = $imgFsBase . $filename;
+	if (is_file($file)) {
+		$mtime = @filemtime($file);
+		if (is_int($mtime) && $mtime > 0) {
+			return '?v=' . rawurlencode((string)$mtime);
+		}
+	}
+	if (defined('SYSTEM_VER')) {
+		return '?v=' . rawurlencode((string)SYSTEM_VER);
+	}
+	return '';
+};
+
+$manifest = array(
+	'name' => $title,
+	'short_name' => $shortName,
+	'description' => $description,
+	'start_url' => $startUrl,
+	'scope' => $scope,
+	'id' => $startUrl,
+	'display' => 'standalone',
+	'theme_color' => '#b77b7b',
+	'background_color' => '#b77b7b',
+	'icons' => array(
+		array(
+			'src' => $imgUrlBase . 'android-chrome-192x192.png' . $assetVer('android-chrome-192x192.png'),
+			'sizes' => '192x192',
+			'type' => 'image/png'
+		),
+		array(
+			'src' => $imgUrlBase . 'android-chrome-256x256.png' . $assetVer('android-chrome-256x256.png'),
+			'sizes' => '256x256',
+			'type' => 'image/png'
+		),
+		array(
+			'src' => $imgUrlBase . 'android-chrome-512x512.png' . $assetVer('android-chrome-512x512.png'),
+			'sizes' => '512x512',
+			'type' => 'image/png'
+		)
+	),
+	'screenshots' => array(
+		array(
+			'src' => $imgUrlBase . 'android-chrome-320x320.png' . $assetVer('android-chrome-320x320.png'),
+			'sizes' => '320x320',
+			'type' => 'image/png',
+			'form_factor' => 'narrow'
+		),
+		array(
+			'src' => $imgUrlBase . 'android-chrome-320x320.png' . $assetVer('android-chrome-320x320.png'),
+			'sizes' => '320x320',
+			'type' => 'image/png',
+			'form_factor' => 'wide'
+		)
+	)
+);
+
+echo json_encode($manifest, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+?>
