14ec073914449a80514eda829f82d71d1dcdbb96
Fraenkiman <frank.pcn@gmail.com>
2026-03-01 16:47:05 +0100

    Addition to #830
    - The setup must also be adjusted, otherwise no valid URLs will be output.

--- setup/tpls/header.tpl.php
+++ setup/tpls/header.tpl.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo LANG_DEFAULT; ?>">
-<head><title>FlatPress <?php echo $lang ['setup'] ['setup']; ?></title>
-		<link href="setup/res/setup.css" type="text/css" rel="stylesheet"></head>
+<head>
+	<title>FlatPress <?php echo $lang ['setup'] ['setup']; ?></title>
+	<link href="setup/res/setup.css" type="text/css" rel="stylesheet">
+</head>
 <body>
 	<div id="rap">
-		<h1 id="header"><img src="<?php echo BLOG_BASEURL; ?>setup/imgs/logo-small.gif" alt="logo-small.gif" title="Logo"><span> Flat</span><span>Press</span> <?php echo $lang ['setup'] ['setup']; ?></h1>
+		<h1 id="header"><img src="setup/imgs/logo-small.gif" alt="logo-small.gif" title="Logo"><span> Flat</span><span>Press</span> <?php echo $lang ['setup'] ['setup']; ?></h1>
 
 		</div>
 
 		<div id="main">
 
-<form class="storycontent" method="post" action="<?php echo BLOG_BASEURL; ?>setup.php">
+<form class="storycontent" method="post" action="setup.php">
--- setup/tpls/step3.tpl.php
+++ setup/tpls/step3.tpl.php
@@ -1,6 +1,24 @@
 <h2><?php echo $l ['head']; ?></h2>
 <div class="post">
 
-<?php echo wpautop(sprintf($l ['descr'], BLOG_BASEURL, BLOG_BASEURL . 'login.php')); ?>
+<?php
+/**
+ * During setup, BLOG_BASEURL may still reflect the pre-configuration value within the same request.
+ * Use the freshly updated config value when available.
+ */
+global $fp_config;
+$base = '';
+
+if (isset($fp_config) && is_array($fp_config)) {
+	$base = (string)($fp_config ['general'] ['www'] ?? '');
+}
+if ($base === '') {
+	$base = (string)BLOG_BASEURL;
+}
+if ($base !== '' && substr($base, -1) !== '/') {
+	$base .= '/';
+}
+echo wpautop(sprintf($l ['descr'], $base, $base . 'login.php'));
+?>
 
 </div>
