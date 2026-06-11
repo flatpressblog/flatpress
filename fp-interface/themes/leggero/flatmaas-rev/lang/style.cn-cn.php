57c91560569a1c2f997a49060d68039073b51ee1
Fraenkiman <frank.pcn@gmail.com>
2026-06-12 01:13:00 +0200

    Update on Chinese support

--- fp-interface/themes/leggero/flatmaas-rev/lang/style.cn-cn.php
+++ fp-interface/themes/leggero/flatmaas-rev/lang/style.cn-cn.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 风格名称：FlatMaas Revisited
- * 风格URI：https://www.flatpress.org/
- * 描述：经典FlatPress外观——在保留原有标准风格的基础上，经过精心改良的现代版设计。
- * 版本：0.705
- * 作者：德鲁多
- 作者网站：https://www.flatpress.org/
+ * Style Name: FlatMaas Revisited
+ * Style URI: https://www.flatpress.org/
+ * Description: FlatPress外观——在保留原有标准风格的基础上，经过精心改良的现代版设计。
+ * Version: 0.705
+ * Author: 作者：德鲁多
+ * Author URI: https://www.flatpress.org/
  */
 ?>
